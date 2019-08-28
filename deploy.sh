3d82d721fb3383b091d2ce42db9bcd03e8631891
resita-alterra <resita@alterra.id>
2019-08-28 15:52:08 +0700

    yes bisa nih

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 sudo docker stop backend
 sudo docker rm backend
 sudo docker rmi resitasriw/backend
-sudo docker run -d --name backend -p 3000:80 resitasriw/backend:latest
+sudo docker run -d --name backend -p 5000:5000 resitasriw/backend:latest
