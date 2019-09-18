776ceea45a02bb66982f98e6c6fd391a9ace6501
resita-alterra <resita@alterra.id>
2019-09-18 22:20:16 +0700

    date

--- blueprints/users/model.py
+++ blueprints/users/model.py
@@ -1,26 +1,30 @@
 from blueprints import db
 from flask_restful import fields
+import datetime
+
 
 class ListUser(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
     user_name = db.Column(db.String(16), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
     alamat = db.Column(db.String(225), nullable=False)
     rekening = db.Column(db.String(30), nullable=False)
     hp = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(50), nullable=False)
-    foto = db.Column(db.String(300), default="https://cdn1.iconfinder.com/data/icons/main-ui-elements-with-colour-bg/512/male_avatar-512.png")
+    foto = db.Column(db.String(
+        300), default="https://cdn1.iconfinder.com/data/icons/main-ui-elements-with-colour-bg/512/male_avatar-512.png")
 
     response_fields = {
-        'id' : fields.Integer,
-        'user_name' : fields.String,
-        'password' : fields.String,
-        'alamat' : fields.String,
-        'rekening' : fields.String,
-        'hp' : fields.String,
-        'email' : fields.String,
-        'foto' : fields.String
+        'id': fields.Integer,
+        'user_name': fields.String,
+        'password': fields.String,
+        'alamat': fields.String,
+        'rekening': fields.String,
+        'hp': fields.String,
+        'email': fields.String,
+        'foto': fields.String
     }
 
     def __init__(self, data):
@@ -33,4 +37,4 @@ class ListUser(db.Model):
         self.foto = data['foto']
 
     # def __repr__(self):
-        # return '<Person %r>' % self.id    
+        # return '<Person %r>' % self.id
